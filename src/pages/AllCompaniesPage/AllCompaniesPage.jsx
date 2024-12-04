ec004b7a19f886ddd2474741a82ce978e0ab8a2a
KiuKralj <urosinter@gmail.com>
2024-12-04 16:07:45 +0100

    All companies page added

--- src/pages/AllCompaniesPage/AllCompaniesPage.jsx
+++ src/pages/AllCompaniesPage/AllCompaniesPage.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./AllCompaniesPage.css";
+
+const URL = "http://localhost:3333/api/companies";
 
 const AllCompaniesPage = () => {
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => setCompanies(data));
+  }, []);
 
   return (
-    <div>
-      
+    <div className="allCompaniesPage-main">
+      <div className="allCompaniesPage-wrapper">
+        {companies.map((company) => {
+          return (
+            <div className="allCompaniesPage-container">
+              <div>
+                <h4>{company.name}</h4>
+                <p>{company.email}</p>
+              </div>
+              <div>{/* <button>X</button> */}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AllCompaniesPage
+export default AllCompaniesPage;
