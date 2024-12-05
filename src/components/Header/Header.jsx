7ca2825de2753cd76e464e32371825697e1bbb65
radovan <urossradovanovic@gmail.com>
2024-12-05 10:51:28 +0100

    Header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import "./Header.css"
+import { Navigate, useNavigate } from 'react-router'
 
 const Header = () => {
+  const navigate = useNavigate();
+  let classSelectedHome; 
+  let classSelectedCompanies; 
+  
+  if(window.location.pathname==='/'){
+    classSelectedHome="header-selected"; 
+    classSelectedCompanies=""; 
+  }else if(window.location.pathname==='/companies'){
+    classSelectedCompanies="header-selected"; 
+    classSelectedHome=""; 
+  }
   return (
-    <div>
-      
-    </div>
+    <>
+      <div className="header-main-div">
+        <img src={require("../../resources/pictures/logo.png")}  onClick={()=>{navigate('/')}}/>
+        <div className="header-pages"> <p className={classSelectedHome} onClick={()=>{navigate('/')}}>Home</p>{" "} <span>|</span> <p className={classSelectedCompanies}  onClick={()=>{navigate('/companies')}}>Companies</p> </div>
+        <div className='header-username'>Username</div>
+      </div>
+    </>
   )
 }
 
