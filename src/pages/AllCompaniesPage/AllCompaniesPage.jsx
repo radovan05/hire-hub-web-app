a9383792b1523178d3dbc4f565867cfa4b34cb10
radovan <urossradovanovic@gmail.com>
2024-12-06 11:54:59 +0100

    universal search done

--- src/pages/AllCompaniesPage/AllCompaniesPage.jsx
+++ src/pages/AllCompaniesPage/AllCompaniesPage.jsx
@@ -2,23 +2,29 @@ import React from "react";
 import { useState, useEffect } from "react";
 import "./AllCompaniesPage.css";
 import { useNavigate } from "react-router";
+import Search from "../../components/Search/Search";
 
 const URL = "http://localhost:3333/api/companies";
 
 const AllCompaniesPage = () => {
   const [companies, setCompanies] = useState([]);
+  const [companiesCopy, setCompaniesCopy] = useState([]);
+
   const navigate = useNavigate(); 
 
   useEffect(() => {
     fetch(URL)
       .then((res) => res.json())
-      .then((data) => setCompanies(data));
+      .then((data) =>{ setCompanies(data);setCompaniesCopy(data);});
+      
   }, []);
+
 
   return (
     <div className="allCompaniesPage-main">
+      <Search data={companiesCopy} setData={setCompaniesCopy} searchBy={'name'} notFilteredData={companies}/>
       <div className="allCompaniesPage-wrapper">
-        {companies.map((company,i) => {
+        {companiesCopy.map((company,i) => {
           return (
             <div key={i} className="allCompaniesPage-container" onClick={() => {
               
