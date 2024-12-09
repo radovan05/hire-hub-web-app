42ceca8949cd396aee54f5611880fc3118d301a4
KiuKralj <urosinter@gmail.com>
2024-12-09 13:16:48 +0100

    create company button and POST method added

--- src/modals/CreateNewCompany/CreateNewCompany.jsx
+++ src/modals/CreateNewCompany/CreateNewCompany.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
+import AllCompaniesPage from "../../pages/AllCompaniesPage/AllCompaniesPage";
+import "./CreateNewCompany.css";
 
-const CreateNewCompany = () => {
+const URL = "http://localhost:3333/api/companies";
+
+const CreateNewCompany = ({ toggleModalOpen, token }) => {
+  const [payLoad, setPayLoad] = React.useState({
+    name: "",
+    email: "",
+  });
+
+  const handleSubmit = () => {
+    console.log(payLoad.name);
+    fetch(URL, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        name: payLoad.name,
+        email: payLoad.email,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="modal" onClick={toggleModalOpen}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <form>
+          <h2>Create New Company:</h2>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            onChange={(e) => {
+              setPayLoad({
+                ...payLoad,
+                name: e.target.value,
+              });
+            }}
+          ></input>
 
-export default CreateNewCompany
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            onChange={(e) => {
+              setPayLoad({
+                ...payLoad,
+                email: e.target.value,
+              });
+            }}
+          />
+
+          <button type="button" onClick={handleSubmit}>
+            Create
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default CreateNewCompany;
--- src/pages/AllCompaniesPage/AllCompaniesPage.jsx
+++ src/pages/AllCompaniesPage/AllCompaniesPage.jsx
@@ -3,33 +3,54 @@ import { useState, useEffect } from "react";
 import "./AllCompaniesPage.css";
 import { useNavigate } from "react-router";
 import Search from "../../components/Search/Search";
+import CreateNewCompany from "../../modals/CreateNewCompany/CreateNewCompany";
 
 const URL = "http://localhost:3333/api/companies";
 
-const AllCompaniesPage = () => {
+const AllCompaniesPage = ({ user }) => {
   const [companies, setCompanies] = useState([]);
   const [companiesCopy, setCompaniesCopy] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch(URL)
       .then((res) => res.json())
-      .then((data) =>{ setCompanies(data);setCompaniesCopy(data);});
-      
+      .then((data) => {
+        setCompanies(data);
+        setCompaniesCopy(data);
+      });
   }, []);
 
+  const toggleModalOpen = () => {
+    setIsModalOpen(!isModalOpen);
+  };
 
   return (
     <div className="allCompaniesPage-main">
-      <Search data={companiesCopy} setData={setCompaniesCopy} searchBy={'name'} notFilteredData={companies}/>
+      <Search
+        data={companiesCopy}
+        setData={setCompaniesCopy}
+        searchBy={"name"}
+        notFilteredData={companies}
+      />
       <div className="allCompaniesPage-wrapper">
-        {companiesCopy.map((company,i) => {
+        {user?.user?.id === 1 ? (
+          <button className="allCompaniesPage-create" onClick={toggleModalOpen}>
+            Create company
+          </button>
+        ) : null}
+
+        {companiesCopy.map((company, i) => {
           return (
-            <div key={i} className="allCompaniesPage-container" onClick={() => {
-              
-                navigate(`/reports/${company.id}`)                
-            }}>
+            <div
+              key={i}
+              className="allCompaniesPage-container"
+              onClick={() => {
+                navigate(`/reports/${company.id}`);
+              }}
+            >
               <div>
                 <h4>{company.name}</h4>
                 <p>{company.email}</p>
@@ -39,6 +60,7 @@ const AllCompaniesPage = () => {
           );
         })}
       </div>
+      {isModalOpen && <CreateNewCompany toggleModalOpen={toggleModalOpen} token={user.accessToken}/>}
     </div>
   );
 };
