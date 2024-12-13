47db1a16c46e27140a23ce0256b75142b65f711e
djstojilkovic <djstojilkovic@gmail.com>
2024-12-13 13:02:49 +0100

    alter css update

--- src/modals/AlterReport/AlterReport.jsx
+++ src/modals/AlterReport/AlterReport.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import { useState,useEffect } from 'react'
-import './AlterReport.css'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./AlterReport.css";
 
-const AlterReport = ({closeModal,rep,token,setLogin}) => {
+const AlterReport = ({ closeModal, rep, token, setLogin }) => {
   const [candidates, setCandidates] = useState([]);
-  const [phase,setPhase]=useState(rep.phase);
-  const [status,setStatus]=useState(rep.status);
-  const [note, setNote]=useState(rep.note);
-  
+  const [phase, setPhase] = useState(rep.phase);
+  const [status, setStatus] = useState(rep.status);
+  const [note, setNote] = useState(rep.note);
 
   useEffect(() => {
     const CANDIDATES_URL = `http://localhost:3333/api/candidates?id=${rep.candidateId}`;
@@ -17,12 +16,19 @@ const AlterReport = ({closeModal,rep,token,setLogin}) => {
   }, []);
 
   return (
-    <div className='alter-body' onClick={()=>{
-      closeModal(false)
-      
-    }}>
-      <div className="alter-main-div" onClick={(e)=>{e.stopPropagation()}}>
-      <div className="report-data">
+    <div
+      className="alter-body"
+      onClick={() => {
+        closeModal(false);
+      }}
+    >
+      <div
+        className="alter-main-div"
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        <div className="report-data">
           <div className="report-data-left">
             <h2>Candidate Information</h2>
 
@@ -42,55 +48,79 @@ const AlterReport = ({closeModal,rep,token,setLogin}) => {
           <div className="report-data-right">
             <h2>Report Information</h2>
             <p>
-              <strong>Phase:</strong> <input type="text" placeholder={rep.phase} onChange={(e)=>{
-                setPhase(e.target.value)
-              }}/>
+              <strong>Phase:</strong>{" "}
+              <input
+                type="text"
+                placeholder={rep.phase}
+                onChange={(e) => {
+                  setPhase(e.target.value);
+                }}
+              />
             </p>
             <p>
-              <strong>Status:</strong> <input type="text" placeholder={rep.status} onChange={(e)=>{
-                setStatus(e.target.value)
-              }}/>
+              <strong>Status:</strong>{" "}
+              <input
+                type="text"
+                placeholder={rep.status}
+                onChange={(e) => {
+                  setStatus(e.target.value);
+                }}
+              />
             </p>
             <p>
               <strong>Interview Date: </strong>
               {new Date(rep.interviewDate).toLocaleString()}
             </p>
+            <strong className="alter-note">Note: </strong>
             <p>
-              <strong>Note: </strong> <textarea cols="30" rows="10" defaultValue={note} onChange={(e)=>{
-                setNote(e.target.value)
-              }}></textarea>
+              <textarea
+                cols="37"
+                rows="5"
+                defaultValue={note}
+                onChange={(e) => {
+                  setNote(e.target.value);
+                }}
+              ></textarea>
             </p>
-            {rep.note!==note || rep.status !==status || rep.phase!==phase ?
-             <button onClick={()=>{
-              fetch(`http://localhost:3333/api/reports/${rep.id}`, {
-                  headers: {
-                    Authorization: `Bearer ${token}`,
-                    "Content-Type": "application/json",
-                  },
-                  method: "put",
-                  body: JSON.stringify({
-                    candidateId: candidates.id,
-                    candidateName: candidates.name,
-                    companyId: rep.companyId,
-                    companyName: rep.companyName,
-                    interviewDate: Date(),
-                    phase: phase,
-                    status: status,
-                    note: note,
-                  }),
-                }).then((res) => res.json()).then(data => {
-                  if (data === "jwt expired") {
-                    setLogin(false);
-                  }
-                });
-                closeModal(false);
-             }}>Submit Change</button>
-            :null}
+            {rep.note !== note ||
+            rep.status !== status ||
+            rep.phase !== phase ? (
+              <button
+                onClick={() => {
+                  fetch(`http://localhost:3333/api/reports/${rep.id}`, {
+                    headers: {
+                      Authorization: `Bearer ${token}`,
+                      "Content-Type": "application/json",
+                    },
+                    method: "put",
+                    body: JSON.stringify({
+                      candidateId: candidates.id,
+                      candidateName: candidates.name,
+                      companyId: rep.companyId,
+                      companyName: rep.companyName,
+                      interviewDate: Date(),
+                      phase: phase,
+                      status: status,
+                      note: note,
+                    }),
+                  })
+                    .then((res) => res.json())
+                    .then((data) => {
+                      if (data === "jwt expired") {
+                        setLogin(false);
+                      }
+                    });
+                  closeModal(false);
+                }}
+              >
+                Submit Change
+              </button>
+            ) : null}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AlterReport
+export default AlterReport;
